34a1eca4a3093df8b332d8427e2306e18a58cfc1
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2024-10-05 15:01:56 +0100

    Update: Just almost complete.

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ form.addEventListener('submit', async (e) => {
   await getData();
 });
 
+// Collect data and store in local storage
+
 async function getData() {
     // Collect all inputs
     const firstname = document.getElementById('firstname').value
@@ -36,3 +38,69 @@ async function getData() {
     window.location.href = 'dashboard.html';
 
 };
+
+// Fetch and display data from the local storage.
+
+function showData() {
+    const displayData = document.getElementById('userDisplay')
+    const userData = JSON.parse(localStorage.getItem('userData'))
+    // console.log(userData);
+
+    // userData.forEach(Data => {
+    //     const displayfname = document.getElementById('firstname')
+    //     displayfname.innerHTML += `${Data.firstname}`
+    // });
+
+    const displayimage = document.getElementById('image');
+    displayimage.src = userData.image;
+    displayimage.style.width = '100px';
+    displayimage.style.height = '100px';
+    displayimage.style.borderRadius = '50%';
+
+
+    const displayfname = document.getElementById('firstname')
+    displayfname.innerHTML += `${userData.firstname}`
+    const displaylname = document.getElementById('lastname');
+    displaylname.innerHTML += userData.lastname;
+    const displayemail = document.getElementById('email')
+    displayemail.innerHTML += userData.email;
+    const displayaddress = document.getElementById('address')
+    displayaddress.innerHTML += userData.address;
+}
+
+// Collect data from API
+
+async function fetchEcomData(){
+
+    const displayData = document.getElementById('displayData')
+
+    try {
+        const fetchedData = await fetch('https://fakestoreapi.com/products');
+        const parsedData = await fetchedData.json()
+        localStorage.setItem('productData', JSON.stringify(parsedData))
+        // console.log(parsedData)
+
+        parsedData.forEach(productData => {
+            displayData.innerHTML +=
+            
+            `
+            <div class="card d-flex m-5" style="width: 18rem;">
+                <img src="${productData.image}" class="card-img-top" alt="ProductPic" id="img" style="height: 18rem; width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${productData.title}</h5>
+                    <h6>Price: ${productData.price}</h6>
+                    <h6>Rating: ${productData.rating.rate}</h6>
+                    <p class="card-text">${productData.description}</p>
+                </div>
+            </div>
+            `
+
+
+        });
+        // const displayimage = document.getElementById('img')
+        // displayimage.src = `${productData.image}`
+        
+    } catch (err) {
+        console.log("Emergency! Check this -->", err)
+    }
+}
